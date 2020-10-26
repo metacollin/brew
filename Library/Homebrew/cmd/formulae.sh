8d921e218a09fa0e8730548eca97a85bb114b2c3
metacollin <metacollin@gmail.com>
2020-10-26 15:25:20 -0600

    Update formulae.sh

--- Library/Homebrew/cmd/formulae.sh
+++ Library/Homebrew/cmd/formulae.sh
@@ -7,7 +7,7 @@
 homebrew-formulae() {
   local formulae="$(
     find "$HOMEBREW_REPOSITORY"/Library/Taps -type d \( -name Casks -o -name spec -o -name lib -o -name cmd -o -name .github \) -prune -false -o -name '*\.rb' |
-    sed -E -e 's/\.rb//' -e 's .*/Taps/(.*)/homebrew-\1/ ' -e 's /Formula/ / ')"
+    sed -E -e 's/\.rb//' -e 's .*/Taps/(.*)/homebrew- \1/ ' -e 's /Formula/ / ')"
   local shortnames="$(echo "$formulae" | cut -d / -f 3)"
   echo -e "$formulae \n $shortnames" | grep -v '^homebrew/' | sort -uf
 }
